10b5ec84356d1200ced6320596163e1df8e6da17
skeret2 <scarlett.zapata@alumnos.ucn.cl>
2022-05-04 23:33:36 -0400

    Feat:Se realizo el diseño de la view.py base,incorporando metodos get-path-post-delete / Test:Se confirmo el funcionamiento mediante las urls creadas para cada vista

--- personalProject/urls.py
+++ personalProject/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from petclub.views import HelloWorld
+from petclub.views import PetListAPIView
+from petclub.views import PetAPIView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('hi', HelloWorld.as_view(), name="helloworld"),
     path('api-auth/', include('rest_framework.urls')),
+    path('petL', PetListAPIView.as_view(), name="petListView"),
+    path('pet', PetAPIView.as_view(), name="petListView"),
 ]
--- petclub/views.py
+++ petclub/views.py
@@ -3,20 +3,30 @@ from django.shortcuts import render
 # modulos de DRF
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
+from rest_framework.generics import RetrieveAPIView
 
 class HelloWorld(APIView):
     def get(self, request): # verbo de la peticion como un metodo
         # logica asociada al endpoint
-        return Response(data="Hello, World !", status=200) # respuesta del servicio
-y en urls.py eliminamos el path del admin y añadimos nuestra view asi:
+        return Response(data="Hola estoy en el get", status=200) # respuesta del servicio
+    
+    def patch(self, request):
+        return Response(data="Hola estoy en el patch", status=200)
+    
+    def delete(self, request):
+        return Response(data="Hola estoy en el delete", status=200)
+    
+    def post(self, request):
+        return Response(data="Hola gente estoy en el post", status=200)
 
-from django.contrib import admin
-from django.urls import path, include
+class PetListAPIView(ListAPIView):
+    def get(self, request):
+        return Response(data="Hola estas son todas mis mascotas", status=200)
 
-from petclub.views import HelloWorld
+class PetAPIView(RetrieveAPIView):
+    def get(self, request):
+        return Response(data="Hola estoy en el post", status=200)
 
-urlpatterns = [
-    path('hi', HelloWorld.as_view(), name="helloworld"),
-    path('api-auth/', include('rest_framework.urls')),
-]
+
 
